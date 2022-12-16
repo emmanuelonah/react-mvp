145aa42fb6adafd661aacbeefaed3dbabbc5b9a3
emmanuelonah <emmajerry2016@gmail.com>
2022-12-16 04:50:14 +0100

    Feature: initial draft of Client Serverless db interface architecture

---
+++ src/helpers/clientdb/index.ts
@@ -0,0 +1,130 @@
+/* eslint-disable class-methods-use-this */
+import 'colors';
+
+import { __DEV__ } from 'utils';
+
+/**
+ * @clientDb
+ *
+ * @clientDbSecurity
+ *
+ * @methods - clientDb methods:
+ *
+ * @create
+ *
+ * @insert
+ *
+ * @find
+ *
+ * @update
+ *
+ * @remove
+ *
+ * @count
+ *
+ * @version - we use IndexDB version 4
+ *
+ * @note - this is not meant to replace you server DBs as the data are only stored on user's computer
+ * for operations that do not require environmental persistency. Also, storage capacity are limited
+ * to available 50% of the hard-disk/local-disk free storage which then follows the LRU policies which might
+ * cause your origin to be evicted. So the reliability of your database presence is not guaranteed.
+ * But there is a storage API ```StorageManager.persist()```that i will expose in this helper class
+ * that will place our database in a permanent location which will be lastly freed incase of QuotaException
+ *
+ * @important this operation is asynchronous and the storage space on you hard-disk memory available to
+ * this is 50% in some browsers and 60% on other browser of the hard-disk and this why this Storage SubClass
+ * leverages over the others like LocalStorage which is synchronous and a fixed storage space of 5mb
+ *
+ * @reference -
+ *  https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Browser_storage_limits_and_eviction_criteria
+ *
+ */
+
+export class ClientDb {
+  private CLIENT_DB!: IDBOpenDBRequest;
+
+  /**
+   * @create
+   *
+   */
+  public create(dbName: string) {
+    ///
+    this.CLIENT_DB = window.indexedDB.open(dbName, 4);
+
+    ///
+    this.CLIENT_DB.onsuccess = (ev) => {
+      if (__DEV__) {
+        console.log(`SUCCESSFULLY OPENED ${dbName.blue} DATABASE`.green);
+        console.log(JSON.stringify(ev));
+      }
+    };
+
+    ///
+    this.CLIENT_DB.onerror = (ev) => {
+      if (__DEV__) {
+        console.log(`FAILED TO OPEN ${dbName.white} DATABASE`.red);
+        console.log(JSON.stringify(ev));
+      }
+    };
+
+    ///
+    return new Promise((resolve, reject) => {
+      if (this.CLIENT_DB.onsuccess) this.CLIENT_DB.onsuccess = (ev) => resolve(ev);
+      else this.CLIENT_DB.onerror = (ev) => reject(ev);
+    });
+  }
+
+  /**
+   * @insert
+   *
+   */
+  public insert() {
+    return new Promise((resolve, reject) => {
+      console.log(resolve, reject);
+    });
+  }
+
+  /**
+   * @find
+   *
+   * @queries
+   *
+   */
+  public find() {
+    return new Promise((resolve, reject) => {
+      console.log(resolve, reject);
+    });
+  }
+
+  /**
+   * @update
+   *
+   */
+  public update() {
+    return new Promise((resolve, reject) => {
+      console.log(resolve, reject);
+    });
+  }
+
+  /**
+   * @remove
+   *
+   */
+  public remove() {
+    return new Promise((resolve, reject) => {
+      console.log(resolve, reject);
+    });
+  }
+
+  /**
+   * @count
+   *
+   * @queries - for different database queries or columns queries
+   *
+   */
+  public count() {
+    return new Promise((resolve, reject) => {
+      console.log(resolve, reject);
+    });
+  }
+}
